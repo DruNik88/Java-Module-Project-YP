a93fcb01d79c4fd10d4b95337d8a6e2da0fdf219
Druzhkov Nikolay <diyavolik007@yandex.ru>
2024-05-30 20:53:14 +0300

    Добавить счётчик количества участников

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,19 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        instructionCount();
+        int number = NumberOfPeoples.numberOfPeoples();
+
     }
+
+    public static void instructionCount() {
+        System.out.println("Добро пожаловать в калькулятор \"По-братски\"");
+        System.out.println("Данный калькулятор предназначен для деления суммы мероприятия \"без обмана\"\n" +
+                "  на равные части между друзьями/товарищами/людьми участвовавщих в оплате.");
+        System.out.println("Вам будет предложено ввести количество друзей/товарищей/людей.");
+        System.out.println("Вам будет предложено ввести наименование товара, а так же его стоимость.");
+        System.out.println("После чего останется получить результат.");
+    }
+
 }
+
